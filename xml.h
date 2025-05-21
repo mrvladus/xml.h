f87b46c600a3e78d2966e602e5b6f043ad52d2bb
mrvladus <mrvladus@yandex.ru>
2025-05-21 13:30:35 +0300

    Update xml.h

--- xml.h
+++ xml.h
@@ -1,26 +1,53 @@
-// MIT License
+/*
 
-// Copyright (c) 2024 Vlad Krupinskii <mrvladus@yandex.ru>
+MIT License
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+Copyright (c) 2024-2025 Vlad Krupinskii <mrvladus@yandex.ru>
 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-// xml.h - Header-only C/C++ library to parse XML.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+------------------------------------------------------------------------------
+
+xml.h - Header-only C/C++ library to parse XML.
+
+------------------------------------------------------------------------------
+
+Features:
+
+- One tiny header file (~400 lines of code)
+- Parses regular (`<tag>Tag Text</tag>`) and self-closing tags (`<tag/>`)
+- Parses tags attributes (`<tag attribute="value" />`)
+- Ignores comments and processing instructions
+
+------------------------------------------------------------------------------
+
+Usage:
+
+Define XML_H_IMPLEMENTATION in ONE file before including "xml.h" to pull functions implementations.
+In other files just include "xml.h".
+
+#define XML_H_IMPLEMENTATION
+#include "xml.h"
+
+------------------------------------------------------------------------------
+
+*/
 
 #ifndef XML_H
 #define XML_H
